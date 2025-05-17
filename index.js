c9fc3c2029bd8c96b900da41018d9c545abfa2e1
absarker <absarker238@gmail.com>
2025-05-17 11:11:32 +0600

    added my cart document

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kggqf0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const client = new MongoClient(uri,{
+const client = new MongoClient(uri, {
     serverAPI: {
         version: ServerApiVersion.v1,
         stric: true,
@@ -22,14 +22,30 @@ const client = new MongoClient(uri,{
 const run = async () => {
     try {
         await client.connect()
-        const  orderCellection=client.db('bistroDB').collection("menu")
-        app.get('/menu',async(req,res)=>{
-            const menu=await orderCellection.find().toArray()
+        const orderCollection = client.db('bistroDB').collection("menu")
+        const reviewCollection = client.db('bistroDB').collection("reviews")
+        const CartCollection = client.db('bistroDB').collection('carts')
+        app.get('/menus', async (req, res) => {
+            const menu = await orderCollection.find().toArray()
             res.send(menu)
+        })
+        app.get('/reviews', async (req, res) => {
+            const review = await reviewCollection.find().toArray()
+            res.send(review)
+        })
+        app.get('/carts', async(req,res)=>{
+            const result=await CartCollection.find().toArray()
+            res.send(result)
+        })
+        app.post('/cart', async (req, res) => {
+            const menuCart = req.body;
+            const result = await CartCollection.insertOne(menuCart)
+            res.send(result)
         })
         await client.db("admin").command({ ping: 1 });
         console.log("Mongodb database successfully connected......")
     }
+
     catch (error) {
         console.log('Error', error)
     }
